cbebd4559727612c8ef960821befb3cd23d8c7d6
Carlos <carlosbolorino@hotmail.com>
2023-03-04 12:20:01 +0100

    two projects settings

---
+++ dapr.microservice.webapi2/Controllers/SaluteController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dapr.microservice.webapi2.Controllers
+{
+    [ApiController]
+    public class SaluteController : ControllerBase
+    {
+        [HttpGet("salute")]
+        public ActionResult<string> Get()
+        {
+            Console.WriteLine("Salute, World.");
+            return "Salute, World";
+        }
+    }
+}
