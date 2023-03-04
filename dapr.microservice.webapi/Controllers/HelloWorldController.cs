6e936910df75484078e17de6f287833efd4bdec6
Carlos <carlosbolorino@hotmail.com>
2023-03-04 10:59:55 +0100

    end chapter 1

---
+++ dapr.microservice.webapi/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dapr.microservice.webapi.Controllers
+{
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet("hello")]
+        public ActionResult<string> Get()
+        {
+            Console.WriteLine("Hello, World.");
+            return "Hello, World";
+        }
+    }
+}
